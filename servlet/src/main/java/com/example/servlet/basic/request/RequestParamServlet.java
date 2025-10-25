352b3a189320fa1402569d185a1ee87f33c65f2e
cylcoder <cylcoder@gmail.com>
2025-10-25 20:24:36 +0900

    Section 3: HTTP 요청 데이터 - POST HTML Form

--- servlet/src/main/java/com/example/servlet/basic/request/RequestParamServlet.java
+++ servlet/src/main/java/com/example/servlet/basic/request/RequestParamServlet.java
@@ -1,9 +1,8 @@
-package com.example.servlet.basic.req;
+package com.example.servlet.basic.request;
 
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletreq;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
