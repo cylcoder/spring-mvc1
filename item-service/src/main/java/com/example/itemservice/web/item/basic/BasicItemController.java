844177c682ce7f063103d184c4de93259dcfc5b6
cylcoder <cylcoder@gmail.com>
2025-11-01 16:00:32 +0900

    Section 8: 상품 등록 처리 - @ModelAttribute

--- item-service/src/main/java/com/example/itemservice/web/item/basic/BasicItemController.java
+++ item-service/src/main/java/com/example/itemservice/web/item/basic/BasicItemController.java
@@ -8,6 +8,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -44,11 +45,39 @@ public class BasicItemController {
     return "basic/addForm";
   }
 
-  @PostMapping("/add")
+//  @PostMapping("/add")
   public String addItemV1(String itemName, int price, Integer quantity, Model model) {
     Item item = new Item(itemName, price, quantity);
     itemRepository.save(item);
     model.addAttribute("item", item);
+    return "basic/item";
+  }
+
+//  @PostMapping("/add")
+  public String addItemV2(@ModelAttribute("item") Item item, Model model) {
+    itemRepository.save(item);
+//    model.addAttribute("item", item); // 자동 추가, 생략 가능
+    return "basic/item";
+  }
+
+  /*
+   * @ModelAttribute name 생략 가능
+   * model.addAttribute(item); 자동 추가, 생략 가능
+   * 생략시 model에 저장되는 name은 클래스명 첫글자만 소문자로 등록 Item -> item
+   */
+//  @PostMapping("/add")
+  public String addItemV3(@ModelAttribute  Item item) {
+    itemRepository.save(item);
+    return "basic/item";
+  }
+
+  /*
+   * @ModelAttribute 자체 생략 가능
+   * model.addAttribute(item) 자동 추가
+   */
+  @PostMapping("/add")
+  public String addItemV4(Item item) {
+    itemRepository.save(item);
     return "basic/item";
   }
 
